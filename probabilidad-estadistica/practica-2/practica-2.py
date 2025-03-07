2d09795442c77406bd224376f3bce5addb0870a8
AndreReyesG <jandre.reyesg@gmail.com>
2025-03-06 18:21:32 -0600

    refactor

--- probabilidad-estadistica/practica-2/practica-2.py
+++ probabilidad-estadistica/practica-2/practica-2.py
@@ -1,15 +1,28 @@
-total_personas = 755
+TOTAL_PERSONAS = 755
 info = [[25, 280], [45, 405]]
+
+# La Matriz es NxN
+N = 2
+
+def sumar_columnas(fila):
+    """Suma todos los elementos de una columna de una n fila.
+    Parameters:
+        fila (int): La fila de las columnas a sumar
+    Return:
+        int: suma de todos los elementos de una columna de una n fila.
+    """
+    return sum(info[i][fila] for i in range(0, N))
+
 # A. Probablidad de que sí use un celular al manejar
-A = sum(info[0]) / total_personas
+A = sum(info[0]) / TOTAL_PERSONAS
 print(f"Problema A: {A}")
 
 # B. Probablidad de que no tenga infracción
-B = (info[0][1] + info[1][1]) / total_personas
+B = sumar_columnas(1) / TOTAL_PERSONAS
 print(f"Problema B: {B}")
 
 # C. Probablidad de que no tenga infracción y que si use celular al manejar
-C = info[0][1] / total_personas
+C = info[0][1] / TOTAL_PERSONAS
 print(f"Problema C: {C}")
 
 # D. Probablidad de que no tenga una infracción o que sí use el celular
@@ -24,10 +37,10 @@ P(B/A) = P(A & B) / P(A)
 
 # E. Probabilidad que sí use el celular dado que sí tiene una infracción (sugerencia: ocupar probablidad condicional)
 # P(sí celular / sí infracción) = P(sí celular & sí infracción) / P(sí infracción)
-#                               = ((sí celular & sí infracción) / total_personas) / (sí infracción / total_personas)
-#                               = ((sí celular & sí infracción) * total_personas) / (sí infracción * total_personas)
+#                               = ((sí celular & sí infracción) / TOTAL_PERSONAS) / (sí infracción / TOTAL_PERSONAS)
+#                               = ((sí celular & sí infracción) * TOTAL_PERSONAS) / (sí infracción * TOTAL_PERSONAS)
 #                               = (sí celular & sí infracción)  / (sí infracción)
-E = info[0][0] / (info[0][0] + info[1][0])
+E = info[0][0] / sumar_columnas(0)
 print(f"Problema E: {E}")
 
 # F. Probabilidad que no tenga una infracción dado que no usa el celular (sugerencia: ocupar probablidad condicional)
